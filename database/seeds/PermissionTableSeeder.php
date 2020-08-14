f85525e5ebdf4ac5fcdb9447982560ebb9662951
vietoba <buixuanviet.dev@gmail.com>
2020-08-14 14:45:19 +0700

    config

--- database/seeds/PermissionTableSeeder.php
+++ database/seeds/PermissionTableSeeder.php
@@ -1,4 +1,5 @@
 <?php
+
 use Illuminate\Database\Seeder;
 use App\User;
 use Spatie\Permission\Models\Role;
@@ -12,14 +13,14 @@ class CreateAdminUserSeeder extends Seeder
 */
 public function run()
 {
-$user = User::create([
-'name' => 'Rahul Shukla',
-'email' => 'admin@rscoder.com',
-'password' => bcrypt('123456')
-]);
-$role = Role::create(['name' => 'Admin']);
-$permissions = Permission::pluck('id','id')->all();
-$role->syncPermissions($permissions);
-$user->assignRole([$role->id]);
+    $user = User::create([
+        'name' => 'Rahul Shukla',
+        'email' => 'admin@rscoder.com',
+        'password' => bcrypt('123456')
+    ]);
+    $role = Role::create(['name' => 'Admin']);
+    $permissions = Permission::pluck('id','id')->all();
+    $role->syncPermissions($permissions);
+    $user->assignRole([$role->id]);
 }
 }
