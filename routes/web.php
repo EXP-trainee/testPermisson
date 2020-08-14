ff2600331381146481814ac0b71e611feaa94cdc
vietoba <buixuanviet.dev@gmail.com>
2020-08-14 11:33:45 +0700

    controller

--- routes/web.php
+++ routes/web.php
@@ -21,10 +21,8 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['middleware' => ['auth']], function() {
+    Route::resource('roles','RoleController');
+    Route::resource('users','UserController');
+    Route::resource('products','ProductController');
+});
